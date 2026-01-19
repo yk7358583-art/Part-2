2db6df2e9faac9ef4e258fe6d38864ce3ecdd589
yk7358583-art <yk7358583@gmail.com>
2026-01-19 09:02:38 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,5 @@
+CourseService courseService = new CourseService();
+CourseMenu courseMenu = new CourseMenu(courseService);
+courseMenu.showMenu();CourseService courseService = new CourseService();
+CourseMenu courseMenu = new CourseMenu(courseService);
+courseMenu.showMenu();
